0ae864a4f590cb363835e89bc31d4f7611164c44
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-02-23 17:28:04 -0300

    feat: Adding dbcontext for infrastructure

---
+++ BookReviewManagement.Infrastructure/Data/BookReviewManagementDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace BookReviewManagement.Infrastructure.Data;
+
+public class BookReviewManagementDbContext(DbContextOptions<BookReviewManagementDbContext> options) : DbContext(options)
+{
+    public DbSet<Book> Books { get; set; }
+    public DbSet<User> Users { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
