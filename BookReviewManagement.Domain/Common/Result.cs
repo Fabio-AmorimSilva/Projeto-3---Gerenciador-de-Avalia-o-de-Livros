7304a0ec8515872f67034f80cc7de82295b7863f
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-02-25 19:20:22 -0300

    feat: Adding Result class in domain

---
+++ BookReviewManagement.Domain/Common/Result.cs
@@ -0,0 +1,43 @@
+﻿namespace BookReviewManagement.Domain.Common;
+
+public class Result
+{
+    public string Message { get; set; } = null!;
+    public bool IsSuccess { get; set; }
+
+    protected Result()
+    {
+    }
+    
+    public static Result Success() => 
+        new()
+        {
+            IsSuccess = true
+        };
+
+    public static Result Error(string message) =>
+        new()
+        {
+            Message = message,
+            IsSuccess = false
+        };
+}
+
+public class Result<T> : Result
+{
+    public T? Data { get; set; }
+    
+    public static Result<T> Success(T? data) 
+        => new()
+        {
+            Data = data,
+            IsSuccess = true
+        };
+
+    public new static Result<T> Error(string message)
+        => new()
+        {
+            Message = message,
+            IsSuccess = false
+        };
+}
