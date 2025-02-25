b6bc96653ed9e84e5cb8f56e154d3b8007fa7e7b
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-02-25 19:19:54 -0300

    refactor: Adjusting namespaces

--- BookReviewManagement.Domain/Common/Entity.cs
+++ BookReviewManagement.Domain/Common/Entity.cs
@@ -1,4 +1,4 @@
-﻿namespace BookReviewManagement.Domain.Entities;
+﻿namespace BookReviewManagement.Domain.Common;
 
 public abstract class Entity
 {
--- BookReviewManagement.Domain/GlobalUsings.cs
+++ BookReviewManagement.Domain/GlobalUsings.cs
@@ -1,5 +1,6 @@
 ﻿// Global using directives
 
+global using BookReviewManagement.Domain.Common;
 global using BookReviewManagement.Domain.Enums;
 global using BookReviewManagement.Domain.Interfaces;
 global using CommunityToolkit.Diagnostics;
