842059429269ad9208c4518dfe7473dfce3c60a2
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-02-26 20:04:16 -0300

    feat: Adding and implemeting PasswordHashService

---
+++ BookReviewManagement.Application/Common/Interfaces/IPasswordHashService.cs
@@ -0,0 +1,6 @@
+﻿namespace BookReviewManagement.Application.Common.Interfaces;
+
+public interface IPasswordHashService
+{
+    string HashPassword(string password);
+}
--- BookReviewManagement.Infrastructure/ServiceCollectionExtensions.cs
+++ BookReviewManagement.Infrastructure/ServiceCollectionExtensions.cs
@@ -5,6 +5,7 @@ public static class ServiceCollectionExtensions
     public static ServiceCollection AddBookReviewManagement(this ServiceCollection services)
     {
         services.AddScoped<IBookReviewManagementDbContext>(provider => provider.GetRequiredService<BookReviewManagementDbContext>());
+        services.AddScoped<IPasswordHashService, PasswordHashService>();
         
         return services;
     }
