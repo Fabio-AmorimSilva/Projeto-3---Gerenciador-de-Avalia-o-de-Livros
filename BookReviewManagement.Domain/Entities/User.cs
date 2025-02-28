400d457b4c73e0ccd78fb028eefee46af1dfd47a
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-02-28 19:56:59 -0300

    feat: Adding Update and UpdatePassword methods in User entity

--- BookReviewManagement.Domain/Entities/User.cs
+++ BookReviewManagement.Domain/Entities/User.cs
@@ -3,18 +3,16 @@
 public class User : Entity
 {
     public const int MaxNameLength = 150;
-    
-    public int Score { get; private set; }
+
     public string Name { get; private set; }
     public string Email { get; private set; }
     public string Password { get; private set; }
-    
+
     private readonly List<Review> _reviews = [];
     public IReadOnlyCollection<Review> Reviews => _reviews;
 
     public User(
-        int score, 
-        string name, 
+        string name,
         string email,
         string password
     )
@@ -23,10 +21,29 @@ public class User : Entity
         Guard.IsLessThanOrEqualTo(name.Length, MaxNameLength, nameof(name));
         Guard.IsNotWhiteSpace(email);
         Guard.IsNotWhiteSpace(password);
-        
-        Score = score;
+
         Name = name;
         Email = email;
+        Password = password;
+    }
+
+    public void Update(
+        string name,
+        string email
+    )
+    {
+        Guard.IsNotWhiteSpace(name);
+        Guard.IsLessThanOrEqualTo(name.Length, MaxNameLength, nameof(name));
+        Guard.IsNotWhiteSpace(email);
+
+        Name = name;
+        Email = email;
+    }
+
+    public void UpdatePassword(string password)
+    {
+        Guard.IsNotWhiteSpace(password);
+        
         Password = password;
     }
 }
