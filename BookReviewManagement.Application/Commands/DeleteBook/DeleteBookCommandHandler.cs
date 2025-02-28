2fb0923094ada06209a2656687daf0d9f7a32606
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-02-28 19:54:48 -0300

    feat: Adding and implementing DeleteBookCommand and handler

---
+++ BookReviewManagement.Application/Commands/DeleteBook/DeleteBookCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace BookReviewManagement.Application.Commands.DeleteBook;
+
+public sealed class DeleteBookCommandHandler(IBookReviewManagementDbContext context) : IRequestHandler<DeleteBookCommand, Result<Unit>>
+{
+    public async Task<Result<Unit>> Handle(DeleteBookCommand request, CancellationToken cancellationToken)
+    {
+        var book = await context.Books.FirstOrDefaultAsync(b => b.Id == request.BookId, cancellationToken);
+
+        if (book is null)
+            return Result<Unit>.Error(ErrorMessages.NotFound<Book>());
+
+        context.Books.Remove(book);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result<Unit>.Success();
+    }
+}
