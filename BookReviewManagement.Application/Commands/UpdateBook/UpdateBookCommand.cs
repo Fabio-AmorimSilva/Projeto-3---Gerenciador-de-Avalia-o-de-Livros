7aa8a54f99e784158b3027bfe4007a26c16debf4
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-03-02 17:57:47 -0300

    feat: Creating and implementing UpdateBookCommand and handler

---
+++ BookReviewManagement.Application/Commands/UpdateBook/UpdateBookCommand.cs
@@ -0,0 +1,58 @@
+﻿namespace BookReviewManagement.Application.Commands.UpdateBook;
+
+public sealed record UpdateBookCommand(
+    Guid BookId,
+    string Title,
+    string Description,
+    string Author,
+    string Publisher,
+    BookGenre Genre,
+    DateTime PublishDate,
+    int Pages
+) : IRequest<Result<Unit>>;
+
+public sealed class UpdateBookCommandValidator : AbstractValidator<UpdateBookCommand>
+{
+    public UpdateBookCommandValidator()
+    {
+        RuleFor(b => b.BookId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateBookCommand.BookId)));
+
+        RuleFor(command => command.Title)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateBookCommand.Title)))
+            .MaximumLength(Book.MaxTitleLength)
+            .WithMessage(ErrorMessages.HasMaxLength(nameof(UpdateBookCommand.Title), Book.MaxTitleLength));
+
+        RuleFor(command => command.Description)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateBookCommand.Description)))
+            .MaximumLength(Book.MaxDescriptionLength)
+            .WithMessage(ErrorMessages.HasMaxLength(nameof(UpdateBookCommand.Description), Book.MaxDescriptionLength));
+
+        RuleFor(command => command.Author)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateBookCommand.Author)))
+            .MaximumLength(Book.MaxAuthorLength)
+            .WithMessage(ErrorMessages.HasMaxLength(nameof(UpdateBookCommand.Author), Book.MaxAuthorLength));
+
+        RuleFor(command => command.Publisher)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateBookCommand.Publisher)))
+            .MaximumLength(Book.MaxPublisherLength)
+            .WithMessage(ErrorMessages.HasMaxLength(nameof(UpdateBookCommand.Publisher), Book.MaxPublisherLength));
+
+        RuleFor(command => command.Genre)
+            .IsInEnum()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateBookCommand.Genre)));
+
+        RuleFor(command => command.PublishDate)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateBookCommand.PublishDate)));
+
+        RuleFor(command => command.Pages)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateBookCommand.Pages)));
+    }
+}
---
+++ BookReviewManagement.Application/Commands/UpdateBook/UpdateBookCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace BookReviewManagement.Application.Commands.UpdateBook;
+
+public sealed class UpdateBookCommandHandler(IBookReviewManagementDbContext context) : IRequestHandler<UpdateBookCommand, Result<Unit>>
+{
+    public async Task<Result<Unit>> Handle(UpdateBookCommand request, CancellationToken cancellationToken)
+    {
+        var book = await context.Books.FirstOrDefaultAsync(b => b.Id == request.BookId, cancellationToken);
+
+        if (book is null)
+            return Result<Unit>.Error(ErrorMessages.NotFound<Book>());
+
+        book.Update(
+            title: request.Title,
+            description: request.Description,
+            author: request.Author,
+            publisher: request.Publisher,
+            genre: request.Genre,
+            pages: request.Pages,
+            publishDate: request.PublishDate
+        );
+
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result<Unit>.Success();
+    }
+}
