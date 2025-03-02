fcaac9d02cb737729408879f6e1aae4f006c6b9b
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-03-02 17:56:01 -0300

    feat: Creating and implementing ListBookReviewsQuery and handler

---
+++ BookReviewManagement.Application/Queries/ListBookReviews/ListBookReviewsQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace BookReviewManagement.Application.Queries.ListBookReviews;
+
+public sealed class ListBookReviewsQueryHandler(IBookReviewManagementDbContext context) : IRequestHandler<ListBookReviewsQuery, Result<IEnumerable<ListBookReviewsViewModel>>>
+{
+    public async Task<Result<IEnumerable<ListBookReviewsViewModel>>> Handle(ListBookReviewsQuery request, CancellationToken cancellationToken)
+    {
+        var reviews = await context.Books
+            .SelectMany(b => b.Reviews.Where(b => b.Id == request.BookId))
+            .Select(r => new ListBookReviewsViewModel
+            {
+                Title = r.Book.Title,
+                Isbn = r.Book.Isbn,
+                Description = r.Description,
+                Score = r.Score
+            })
+            .ToListAsync(cancellationToken);
+        
+        return Result<IEnumerable<ListBookReviewsViewModel>>.Success(reviews);
+    }
+}
