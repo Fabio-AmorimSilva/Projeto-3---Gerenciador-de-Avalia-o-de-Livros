142457dfbf4c9ea52d47228ce6460e8bba15f51f
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-03-04 18:45:49 -0300

    feat: Adding interceptor in dependency injection

--- BookReviewManagement.Infrastructure/ServiceCollectionExtensions.cs
+++ BookReviewManagement.Infrastructure/ServiceCollectionExtensions.cs
@@ -1,10 +1,16 @@
-﻿namespace Microsoft.Extensions.DependencyInjection;
+﻿namespace BookReviewManagement.Infrastructure;
 
 public static class ServiceCollectionExtensions
 {
     public static ServiceCollection AddBookReviewManagement(this ServiceCollection services)
     {
+        services.AddDbContext<BookReviewManagementDbContext>((sp, options) =>
+        {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+        });
+        
         services.AddScoped<IBookReviewManagementDbContext>(provider => provider.GetRequiredService<BookReviewManagementDbContext>());
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
         services.AddScoped<IPasswordHashService, PasswordHashService>();
         
         return services;
