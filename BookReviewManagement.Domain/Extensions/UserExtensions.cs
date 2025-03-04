03c7affbb5ac9eb656d9959921a7a895f182f700
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-03-04 19:09:10 -0300

    feat: Adding new extension method for user entity

---
+++ BookReviewManagement.Domain/Extensions/UserExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace BookReviewManagement.Domain.Extensions;
+
+public static class UserExtensions
+{
+    public static IEnumerable<Claim> GetClaims(this User user)
+    {
+        return
+        [
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.Name),
+            new Claim(ClaimTypes.Email, user.Email)
+        ];
+    }
+}
