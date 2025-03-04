33c307a900958634ac76d6f2b9f778f471ba5dc0
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-03-04 18:45:09 -0300

    feat: Adding new interceptor for auditable fields in entities

---
+++ BookReviewManagement.Infrastructure/Interceptors/AuditableEntityInterceptor.cs
@@ -0,0 +1,36 @@
+﻿namespace BookReviewManagement.Infrastructure.Interceptors;
+
+public sealed class AuditableEntityInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        SetCreateInfo(eventData.Context);
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        SetCreateInfo(eventData.Context);
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private void SetCreateInfo(DbContext? context)
+    {
+        if (context is null)
+            return;
+
+
+        if (context.ChangeTracker.Entries().All(e => e.State != EntityState.Added))
+            return;
+
+        var createdEntities = context.ChangeTracker
+            .Entries<IAuditableEntity>()
+            .Where(e => e.State == EntityState.Added);
+
+        foreach (var entry in createdEntities)
+        {
+            entry.Entity.CreatedAt = DateTime.Now;
+        }
+    }
+}
