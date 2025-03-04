cd47725b5311bc53f82f7cd5d40c96395fcf40cf
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-03-04 19:28:26 -0300

    feat: Creating fail fast validation using IPipelineBehaviour for commands

---
+++ BookReviewManagement.Application/Behaviours/ValidationBehavior.cs
@@ -0,0 +1,22 @@
+﻿namespace BookReviewManagement.Application.Behaviours;
+
+public class ValidationBehavior<TRequest, TResponse>(IValidator<TRequest>? validator = null) 
+    : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken
+    )
+    {
+        if(validator is null)
+            return await next();
+        
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+        
+        if(!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+        
+        return await next();
+    }
+}
---
+++ BookReviewManagement.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace BookReviewManagement.Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static ServiceCollection AddApplication(this ServiceCollection services)
+    {
+        services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.TryAddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        
+        return services;
+    }
+}
