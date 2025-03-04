e4f45664bf6b85f4c0bd8ee1f91ae55170cc5956
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-03-04 19:53:51 -0300

    feat: Adding command and handler for login

---
+++ BookReviewManagement.Application/Commands/Login/LoginCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace BookReviewManagement.Application.Commands.Login;
+
+public sealed class LoginCommandHandler(
+    IBookReviewManagementDbContext context,
+    IPasswordHashService passwordService,
+    ITokenService tokenService
+) : IRequestHandler<LoginCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+        var hashed = passwordService.HashPassword(request.Password);
+
+        var user = await context.Users.FirstOrDefaultAsync(
+            u => u.Email == request.Email &&
+                 u.Password == hashed, cancellationToken);
+        
+        if(user is null)
+            return Result<string>.Error(ErrorMessages.EmailOrPasswordAreIncorrect());
+
+        var token = tokenService.GenerateToken(user);
+        
+        return Result<string>.Success(token);
+    }
+}
