cc1709269d43baaa4d66a321d7de3f855e4bf290
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-03-08 18:31:41 -0300

    feat: Adding index page for listing users

---
+++ BookReviewManagement.Blazor/Components/Pages/Users/Index.razor.cs
@@ -0,0 +1,23 @@
+﻿namespace BookReviewManagement.Blazor.Components.Pages.Users;
+
+public partial class Index : ComponentBase
+{
+    [Inject]
+    private IMediator Mediator { get; set; }
+    [Inject]
+    private NavigationManager Navigation { get; set; }
+    [Inject]
+    private ISnackbar Snackbar { get; set; }
+
+    [Inject]
+    private IDialogService DialogService { get; set; }
+    
+    public IEnumerable<ListUsersViewModel> Users = [];
+    
+    protected override async Task OnInitializedAsync()
+    {
+        var users = await Mediator.Send(new ListUsersQuery());
+        
+        Users = users.Data ?? [];
+    }
+}
