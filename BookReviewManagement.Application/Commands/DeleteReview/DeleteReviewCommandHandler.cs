fd1e58a79463c846c75703ad479a7a123552a27d
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-03-11 20:01:01 -0300

    feat: Adding DeleteReviewCommand and handler

---
+++ BookReviewManagement.Application/Commands/DeleteReview/DeleteReviewCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace BookReviewManagement.Application.Commands.DeleteReview;
+
+public sealed class DeleteReviewCommandHandler(IBookReviewManagementDbContext context) : IRequestHandler<DeleteReviewCommand, Result<Unit>>
+{
+    public async Task<Result<Unit>> Handle(DeleteReviewCommand request, CancellationToken cancellationToken)
+    {
+        var book = await context.Books
+            .Include(b => b.Reviews.Where(r => r.Id == request.ReviewId))
+            .FirstOrDefaultAsync(cancellationToken);
+        
+        if (book is null)
+            return Result<Unit>.Error(ErrorMessages.NotFound<Book>());
+        
+        var review = book.GetReview(request.ReviewId);
+
+        if (review is null)
+            return Result<Unit>.Error(ErrorMessages.NotFound<Review>());
+
+        book.DeleteReview(review);
+        
+        await context.SaveChangesAsync(cancellationToken);
+        
+        return Result<Unit>.Success();
+    }
+}
