debf33670044dbfa54b129f827e9ac4085019a0c
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-03-11 20:01:20 -0300

    feat: Adding UpdateReviewCommand and handler

---
+++ BookReviewManagement.Application/Commands/UpdateReview/UpdateReviewCommand.cs
@@ -0,0 +1,27 @@
+﻿namespace BookReviewManagement.Application.Commands.UpdateReview;
+
+public sealed record UpdateReviewCommand(
+    Guid ReviewId,
+    int Score,
+    string Description
+) : IRequest<Result<Unit>>;
+
+public sealed class UpdateReviewCommandValidator : AbstractValidator<UpdateReviewCommand>
+{
+    public UpdateReviewCommandValidator()
+    {
+        RuleFor(command => command.ReviewId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateReviewCommand.ReviewId)));
+
+        RuleFor(command => command.Score)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateReviewCommand.Score)));
+
+        RuleFor(command => command.Description)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateReviewCommand.Description)))
+            .MaximumLength(Review.MaxDescriptionLength)
+            .WithMessage(ErrorMessages.HasMaxLength(nameof(UpdateReviewCommand.Description), Review.MaxDescriptionLength));
+    }
+}
