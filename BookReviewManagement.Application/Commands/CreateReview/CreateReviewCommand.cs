ffaf111b00d9dee91bc71dead9beb176df441d41
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-03-11 20:00:48 -0300

    feat: Adding CreateReviewCommand and handler

---
+++ BookReviewManagement.Application/Commands/CreateReview/CreateReviewCommand.cs
@@ -0,0 +1,33 @@
+﻿namespace BookReviewManagement.Application.Commands.CreateReview;
+
+public sealed record CreateReviewCommand(
+    Guid BookId,
+    Guid UserId,
+    int Score,
+    string Description
+) : IRequest<Result<Guid>>;
+
+
+public sealed class CreateReviewCommandValidator : AbstractValidator<CreateReviewCommand>
+{
+    public CreateReviewCommandValidator()
+    {
+        RuleFor(command => command.Score)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateReviewCommand.Score)));
+        
+        RuleFor(command => command.Description)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateReviewCommand.Description)))
+            .MaximumLength(Review.MaxDescriptionLength)
+            .WithMessage(ErrorMessages.HasMaxLength(nameof(CreateReviewCommand.Description), Review.MaxDescriptionLength));
+        
+        RuleFor(command => command.UserId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateReviewCommand.UserId)));
+        
+        RuleFor(command => command.BookId)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(CreateReviewCommand.BookId)));
+    }
+}
---
+++ BookReviewManagement.Application/Commands/CreateReview/CreateReviewCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace BookReviewManagement.Application.Commands.CreateReview;
+
+public sealed class CreateReviewCommandHandler(IBookReviewManagementDbContext context) : IRequestHandler<CreateReviewCommand, Result<Guid>>
+{
+    public async Task<Result<Guid>> Handle(CreateReviewCommand request, CancellationToken cancellationToken)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(u => u.Id == request.UserId, cancellationToken);
+
+        if (user is null)
+            return Result<Guid>.Error(ErrorMessages.NotFound<User>());
+        
+        var book = await context.Books.FirstOrDefaultAsync(b => b.Id == request.BookId, cancellationToken);
+
+        if (book is null)
+            return Result<Guid>.Error(ErrorMessages.NotFound<Book>());
+        
+        var review = new Review(
+            user: user,
+            book: book,
+            score: request.Score,
+            description: request.Description
+        );
+        
+        user.AddReview(review);
+        
+        await context.SaveChangesAsync(cancellationToken);
+        
+        return Result<Guid>.Success(review.Id);
+    }
+}
