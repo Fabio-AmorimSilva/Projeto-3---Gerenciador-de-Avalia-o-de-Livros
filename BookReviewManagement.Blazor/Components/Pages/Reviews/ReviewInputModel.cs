d7efb9dc4a884009c825dcfb63583abe38a5bdf9
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-03-15 17:03:58 -0300

    feat: Adding input models for books and reviews

---
+++ BookReviewManagement.Blazor/Components/Pages/Reviews/ReviewInputModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BookReviewManagement.Blazor.Components.Pages.Reviews;
+
+public sealed record ReviewInputModel
+{
+    public Guid BookId { get; set; }
+    public int Score { get; set; }
+    public string Description { get; set; } = null!;
+}
