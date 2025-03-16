c102f79132c90f53befd021a0b53dd46ff837194
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-03-16 17:04:50 -0300

    feat: Creating logout page

---
+++ BookReviewManagement.Blazor/Components/Pages/Users/Logout.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace BookReviewManagement.Blazor.Components.Pages.Users;
+
+public partial class Logout : ComponentBase
+{
+    [Inject]
+    public ILocalStorageService LocalStorageService { get; set; }
+    
+    [Inject]
+    public TokenAuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+    
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+    
+    protected override async Task OnInitializedAsync()
+    { 
+        await LocalStorageService.RemoveItemAsync("Bearer");
+        await LocalStorageService.RemoveItemAsync("userEmail");
+        AuthenticationStateProvider.StateChanged();
+        NavigationManager.NavigateTo("/users/login");
+    }
+}
