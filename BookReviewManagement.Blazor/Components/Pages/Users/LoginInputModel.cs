e6532aac46f11123e288ee64c2e83a93712b2904
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-03-16 17:15:38 -0300

    feat: Creating LoginInputModel

---
+++ BookReviewManagement.Blazor/Components/Pages/Users/LoginInputModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BookReviewManagement.Blazor.Components.Pages.Users;
+
+public sealed record LoginInputModel
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
