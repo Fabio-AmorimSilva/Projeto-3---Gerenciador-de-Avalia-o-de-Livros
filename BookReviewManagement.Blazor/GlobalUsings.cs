f707a42789ff8e2bc74060e5410ffe35ff7265ae
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-03-16 17:07:55 -0300

    feat: Adding new namespaces

--- BookReviewManagement.Blazor/GlobalUsings.cs
+++ BookReviewManagement.Blazor/GlobalUsings.cs
@@ -1,21 +1,33 @@
 ﻿// Global using directives
 
+global using System.Net.Http.Headers;
+global using System.Security.Claims;
+global using System.Text.Json;
+global using Blazored.LocalStorage;
 global using BookReviewManagement.Application;
 global using BookReviewManagement.Application.Commands.CreateBook;
+global using BookReviewManagement.Application.Commands.CreateReview;
 global using BookReviewManagement.Application.Commands.CreateUser;
 global using BookReviewManagement.Application.Commands.DeleteBook;
 global using BookReviewManagement.Application.Commands.DeleteReview;
 global using BookReviewManagement.Application.Commands.DeleteUser;
+global using BookReviewManagement.Application.Commands.Login;
 global using BookReviewManagement.Application.Commands.UpdateUser;
 global using BookReviewManagement.Application.Queries.GetUser;
 global using BookReviewManagement.Application.Queries.ListBookReviews;
 global using BookReviewManagement.Application.Queries.ListBooks;
+global using BookReviewManagement.Application.Queries.ListUserReviews;
 global using BookReviewManagement.Application.Queries.ListUsers;
+global using BookReviewManagement.Blazor.Auth;
 global using BookReviewManagement.Blazor.Components;
+global using BookReviewManagement.Blazor.Components.Layout;
 global using BookReviewManagement.Domain.Enums;
 global using BookReviewManagement.Infrastructure;
 global using MediatR;
 global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Authorization;
 global using Microsoft.AspNetCore.Components.Forms;
 global using MudBlazor;
 global using MudBlazor.Services;
+global using BookReviewManagement.Blazor.Components.Layout;
+global using Microsoft.AspNetCore.Components.Authorization;
