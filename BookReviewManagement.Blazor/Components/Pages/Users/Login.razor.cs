43ecbbb12ba1f7e3f7516ca40ca170f11a392b4d
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-03-16 17:04:39 -0300

    feat: Creating login page

---
+++ BookReviewManagement.Blazor/Components/Pages/Users/Login.razor.cs
@@ -0,0 +1,65 @@
+﻿namespace BookReviewManagement.Blazor.Components.Pages.Users;
+
+public partial class Login : ComponentBase
+{
+    [Inject]
+    public IMediator Mediator { get; set; }
+    
+    [Inject]
+    public NavigationManager Navigation { get; set; }
+    
+    [Inject]
+    public ISnackbar Snackbar { get; set; }
+
+    [Inject]
+    public ILocalStorageService LocalStorage { get; set; }
+    
+    private LoginInputModel LoginInputModel { get; set; } = new();
+    
+    [Inject]
+    public TokenAuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+    
+    public bool IsShow;
+    public InputType PasswordInputType { get; set; } = InputType.Password;
+    public string PasswordInputIcon = Icons.Material.Filled.VisibilityOff;
+    
+    private async Task OnValidSubmitAsync(EditContext editContext)
+    {
+        if (editContext.Model is LoginInputModel model)
+        {
+            var command = new LoginCommand(
+                Email: model.Email,
+                Password: model.Password
+            );
+            
+            var response = await Mediator.Send(command);
+
+            if (!response.IsSuccess)
+            {
+                Snackbar.Add($"{response.Message}", Severity.Error);
+            }
+            else
+            {
+                await LocalStorage.SetItemAsync("Bearer", response.Data);
+                await LocalStorage.SetItemAsync("userEmail", model.Email);
+                AuthenticationStateProvider.StateChanged();
+            }
+        }
+    }
+    
+    public void PasswordTextField()
+    {
+        if (IsShow)
+        {
+            IsShow = false;
+            PasswordInputIcon = Icons.Material.Filled.VisibilityOff;
+            PasswordInputType = InputType.Password;
+        }
+        else
+        {
+            IsShow = true;
+            PasswordInputIcon = Icons.Material.Filled.Visibility;
+            PasswordInputType = InputType.Text;
+        }
+    }
+}
