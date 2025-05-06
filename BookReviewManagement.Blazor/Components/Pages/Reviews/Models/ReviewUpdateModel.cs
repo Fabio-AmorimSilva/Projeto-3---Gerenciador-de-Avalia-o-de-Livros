9228d6d4d652ccee27ebecb5e02f77b37c77b350
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-06 19:59:00 -0300

    feat: Adding Update Review page

---
+++ BookReviewManagement.Blazor/Components/Pages/Reviews/Models/ReviewUpdateModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BookReviewManagement.Blazor.Components.Pages.Reviews.Models;
+
+public sealed record ReviewUpdateModel
+{
+    public int Score { get; set; }
+    public string Description { get; set; } = null!;
+}
---
+++ BookReviewManagement.Blazor/Components/Pages/Reviews/Update.razor.cs
@@ -0,0 +1,47 @@
+﻿using BookReviewManagement.Application.Queries.GetReview;
+
+namespace BookReviewManagement.Blazor.Components.Pages.Reviews;
+
+public partial class Update : ComponentBase
+{
+    [Inject]
+    public NavigationManager Navigation { get; set; }
+    
+    [Inject]
+    public IMediator Mediator { get; set; }
+    
+    [Inject]
+    public IDialogService DialogService { get; set; }
+    
+    [Inject]
+    public ISnackbar Snackbar { get; set; }
+    
+    [Parameter]
+    public Guid ReviewId { get; set; }
+    
+    public ReviewUpdateModel ReviewUpdateModel { get; set; } = new();
+
+    protected override async Task OnInitializedAsync()
+    {
+        var response = await Mediator.Send(new GetReviewQuery(ReviewId));
+        
+        if(!response.IsSuccess)
+            Snackbar.Add($"Error: {response.Message}", Severity.Error);
+
+        var review = response.Data;
+
+        if(review is not null)
+        {
+            ReviewUpdateModel = new ReviewUpdateModel
+            {
+                Description = review.Description,
+                Score = review.Score
+            };
+        }
+    }
+
+    private async Task OnValidSubmitAsync()
+    {
+        
+    }
+}
