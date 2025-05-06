71278bf69ba4d633d7389be130006bd09443374b
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-06 19:58:35 -0300

    feat: Adding GetReviewQuery and handler

---
+++ BookReviewManagement.Application/Queries/GetReview/GetReviewQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace BookReviewManagement.Application.Queries.GetReview;
+
+public sealed class GetReviewQueryHandler(IBookReviewManagementDbContext context) : IRequestHandler<GetReviewQuery, Result<GetReviewViewModel>>
+{
+    public async Task<Result<GetReviewViewModel>> Handle(GetReviewQuery request, CancellationToken cancellationToken)
+    {
+        var review = await context.Reviews
+            .AsNoTracking()
+            .FirstOrDefaultAsync(r => r.Id == request.ReviewId, cancellationToken);
+
+        if (review is null)
+            return Result<GetReviewViewModel>.Error(ErrorMessages.NotFound<Review>());
+
+        return Result<GetReviewViewModel>.Success(new GetReviewViewModel
+        {
+            Description = review.Description,
+            Score = review.Score
+        });
+    }
+}
---
+++ BookReviewManagement.Application/Queries/GetReview/GetReviewViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BookReviewManagement.Application.Queries.GetReview;
+
+public sealed record GetReviewViewModel
+{
+    public required int Score { get; init; }
+    public required string Description { get; init; }
+}
